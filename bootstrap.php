6b1a392d5d07634ee6a666bbbe630721790db9f0
Alex Standiford <a@alexstandiford.com>
2021-05-09 11:22:33 -0700

    Beer CPT refactor

--- bootstrap.php
+++ bootstrap.php
@@ -38,64 +38,9 @@ function beer() {
 }
 
 /**
- * Setup Custom Post Types
+ * Setup Custom Post Types, taxonomies, and meta specific to this post type
  */
-beer()->custom_post_types()->add( 'beer', [
-	'type'        => 'beer',
-	'name'        => 'Beer',
-	'description' => 'Lists beers as a custom post type',
-	'args'        => [
-		'public'          => true,
-		'capability_type' => 'post',
-		'can_export'      => true,
-		'show_in_rest'    => true,
-		'supports'        => [ 'title', 'editor', 'excerpt', 'custom-fields' ],
-		'labels'          => [
-			'name'               => beer()->__( 'Beers' ),
-			'singular_name'      => beer()->__( 'Beer' ),
-			'label'              => beer()->__( 'Beers' ),
-			'add_new'            => beer()->__( 'Add new beer' ),
-			'add_new_item'       => beer()->__( 'Add new beer' ),
-			'new_item'           => beer()->__( 'New beer' ),
-			'edit_item'          => beer()->__( 'Edit beer' ),
-			'view_item'          => beer()->__( 'View beer' ),
-			'all_items'          => beer()->__( 'All beer' ),
-			'search_items'       => beer()->__( 'Search beers' ),
-			'not_found'          => beer()->__( 'No beer found. Sad.' ),
-			'not_found_in_trash' => beer()->__( 'No beer in trash.' ),
-		],
-	],
-] );
-
-/**
- * Setup Taxonomies
- */
-beer()->taxonomies()->add( 'taxonomy', [
-	'post_type'   => 'beer',
-	'id'          => 'style',
-	'description' => 'Beer style',
-	'name'        => 'Styles',
-	'args'        => [
-		'public'       => true,
-		'show_in_rest' => true,
-		'hierarchical' => true,
-		'query_var'    => true,
-		'rewrite'      => [ 'slug' => 'style' ],
-		'labels'       => [
-			'name'              => beer()->__( 'Styles' ),
-			'singular_name'     => beer()->__( 'Style' ),
-			'search_items'      => beer()->__( 'Search Styles' ),
-			'all_items'         => beer()->__( 'All Styles' ),
-			'parent_item'       => beer()->__( 'Parent Style' ),
-			'parent_item_colon' => beer()->__( 'Parent Style:' ),
-			'edit_item'         => beer()->__( 'Edit Style' ),
-			'update_item'       => beer()->__( 'Update Style' ),
-			'add_new_item'      => beer()->__( 'Add New Style' ),
-			'new_item_name'     => beer()->__( 'New Beer Style' ),
-			'not_found'         => beer()->__( 'No styles found' ),
-		],
-	],
-] );
+beer()->custom_post_types()->add( 'beer', 'Beer_List\Custom_Post_Types\Beer' );
 
 /**
  * Setup Scripts
@@ -120,27 +65,6 @@ beer()->loaders()->add( 'colors', [
 	'registry' => 'Beer_List\Loaders\Colors',
 ] );
 
-beer()->meta()->add( 'srm', [
-	'key'                     => 'srm',
-	'subtype'                 => 'beer',
-	'description'             => 'The SRM (color) of a beer',
-	'name'                    => 'SRM',
-	'default_value'           => 25,
-	'type'                    => 'post',
-	'show_in_rest'            => true,
-	'field_type'              => 'integer',
-	'single'                  => true,
-	'sanitize_callback'       => function ( $meta_value, $meta_key, $object_type ) {
-		$valid_color = ! empty( beer()->colors()->filter( [ 'srm' => $meta_value ] ) );
-
-		if ( true === $valid_color ) {
-			return $meta_value;
-		}
-
-		return $this->default_value;
-	},
-	'has_permission_callback' => fn () => current_user_can( 'edit_posts' ),
-] );
 
 beer()->rest_endpoints()->add( 'srm', [
 	'endpoint_callback'       => fn ( \WP_REST_Request $request ) => array_values( (array) beer()->colors() ),
@@ -148,37 +72,4 @@ beer()->rest_endpoints()->add( 'srm', [
 	'rest_namespace'          => 'beer-list/v1',
 	'route'                   => 'srm',
 	'args'                    => [ 'methods' => 'GET' ],
-] );
-
-/**
- * Setup ABV
- */
-beer()->meta()->add('abv', [
-	'key'                     => 'abv',
-	'subtype'                 => 'beer',
-	'description'             => 'The alcohol by volume of a beer',
-	'name'                    => 'ABV',
-	'default_value'           => 12,
-	'type'                    => 'post',
-	'show_in_rest'            => true,
-	'field_type'              => 'number',
-	'single'                  => true,
-	'has_permission_callback' => fn () => current_user_can( 'edit_posts' ),
-] );
-
-/**
- * Setup IBU
- */
-beer()->meta()->add('ibu', [
-	'key'                     => 'ibu',
-	'subtype'                 => 'beer',
-	'description'             => 'The international bitterness units of a beer',
-	'name'                    => 'ABV',
-	'default_value'           => 12,
-	'type'                    => 'post',
-	'show_in_rest'            => true,
-	'field_type'              => 'integer',
-	'single'                  => true,
-	'sanitize_callback'       => 'absint',
-	'has_permission_callback' => fn () => current_user_can( 'edit_posts' ),
 ] );
